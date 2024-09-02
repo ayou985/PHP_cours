dcfcd05f67317f3dc5f50bf2bcdba85f9a6c3827
ayou985 <chelhayoub6@gmail.com>
2024-09-02 13:56:33 +0200

    fin du cours bases

--- index.php
+++ index.php
@@ -20,6 +20,28 @@
     </nav>
 
     <h1>Hello !</h1>
+    <p>Bienvenue, dans le monde de PHP</p>
+    <?php
+    echo '<h2> Les balises </h2>';
+    echo '<p class="fst-italic text-secondary"> Ici je suis dans les balises PHP.</p>';
+    ?>
+    <p>
+        Pour exécuter un script PHP, il faut l'écrire dans un fichier ayant l'extension .php et dans un passage PHP. Pour ouvrir un passage en PHP on utilise : < ?php pour le refermer on utilise ?>.
+            En dehors des balises d'ouverture et de fermeture du PHP, il est possible d'écrire du HTML.
+    </p>
+    <?php
+    echo '<p>Echo est une instruction qui permet d\'afficher dans le navigateur. Toutes les instructions se termiennt par un ";". Dans un echo nous pouvons mettre aussi du HTML.</p>';
+
+    ?>
+    <h3>Différents affichage</h3>
+    <?php
+    echo '<p> valeur 1 </p>', '<p> valeur 2</p>';
+    print '<p> La différence majeure avec echo est que print n\'accepte qu\'un seul argument et retourne toujours 1.';
+    ?>
+    <ul>
+        <li>print_r, affiche des informations à propos d'une variable, de manière à ce qu'elle soit lisible.</li>
+        <li>var_dump, affiche les informations structurées d'une variable, y compris son type et sa valeur.</li>
+    </ul>
 </body>
 
 </html>
