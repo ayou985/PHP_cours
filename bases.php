7c86c0b69bbe71dce5b5a5e179d4bbfeb04a3d46
ayou985 <chelhayoub6@gmail.com>
2024-09-02 12:21:42 +0200

    voici le cours

--- bases.php
+++ bases.php
@@ -4,9 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Les bases en PHP</title>
+    <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="/public/css/style.css">
     <link rel="stylesheet" href="/public/css/style.css">
 </head>
 
@@ -14,8 +15,8 @@
     <nav class="navbar bg-dark">
         <div class="container">
             <a class="navbar-brand text-light" href="#">
-                <img src="/public/img/logo_poleS.png" alt="Bootstrap" width="30">Les bases en PHP</a>
-            <a href="/bases.php">Les bases</a>
+                <img src="/public/img/logo_poleS.webp" alt="Bootstrap" width="30">Les bases en PHP</a>
+            <a href="/index.php">Accueil</a>
         </div>
     </nav>
 
